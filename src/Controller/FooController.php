e223630b3f9ad72ec93364603772cba498e93dac
Anna-wild <agburda26@gmail.com>
2018-04-21 16:57:13 +0300

    21/04/2018

--- src/Controller/FooController.php
+++ src/Controller/FooController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -15,5 +17,26 @@ class FooController extends Controller
         return $this->render('foo/index.html.twig', [
             'controller_name' => 'FooController',
         ]);
+    }
+
+    /**
+     * @Route("/", name="hello_page")
+     */
+    public function hello(Request $request)
+    {
+        $someName=$request->query->get("name");
+
+        return $this->render("hello_page.html.twig", ['someAwesomeVariable'=>' welcome from FooController', 'getName'=>$someName]);
+    }
+
+    /**
+     * @Route("/blog/{page}", name="single_page", requirements={"page"="[A-Za-z]+"})
+     * @param string $page
+    */
+    public function page(string $page)
+    {
+    return $this->render('foo/page.html.twig',[
+        'blogPage'=>$page
+    ]);
     }
 }
